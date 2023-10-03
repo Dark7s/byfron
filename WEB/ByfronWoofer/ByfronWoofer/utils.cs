9520d91f06488516cf0bb682c0eb8a7f4ef66314
DVL <60398155+dvlq@users.noreply.github.com>
2023-10-03 17:16:32 +0200

    beifwon

---
+++ WEB/ByfronWoofer/ByfronWoofer/utils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ByfronWoofer
+{
+    internal class utils
+    {
+        public static void RunAsProcess(string Code)
+        {
+            Process process = Process.Start(new ProcessStartInfo("cmd.exe", "/c " + Code)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
+            process?.WaitForExit();
+            process?.Close();
+        }
+
+        private static readonly Random random = new Random(Environment.TickCount);
+        public static string RandomString(int length)
+        {
+            char[] array = "abcdefghlijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToArray();
+            string text = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                text += array[random.Next(array.Length)].ToString();
+            }
+            return text;
+        }
+
+        public static string RandomNumberString(int length)
+        {
+            char[] array = "0123456789".ToArray();
+            string text = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                text += array[random.Next(array.Length)].ToString();
+            }
+            return text;
+        }
+
+        public static void DeleteDirectory(DirectoryInfo dir)
+        {
+            foreach (FileInfo f in dir.GetFiles()) f.Delete();
+            foreach (DirectoryInfo SubDir in dir.GetDirectories()) SubDir.Delete(true);
+        }
+        public static bool ByteArrayToFile(string fileName, byte[] byteArray)
+        {
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(byteArray, 0, byteArray.Length);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+                return false;
+            }
+        }
+    }
+}
